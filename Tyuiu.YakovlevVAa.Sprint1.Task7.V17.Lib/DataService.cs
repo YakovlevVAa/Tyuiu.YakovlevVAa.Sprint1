6196c046117b862c543fd97b12c62cfba6beb8d0
Виктор Яковлев <Tyuiu.YakovlevVA@outlook.com>
2024-10-04 23:22:00 +0500

    Task 7

--- Tyuiu.YakovlevVAa.Sprint1.Task7.V17.Lib/DataService.cs
+++ Tyuiu.YakovlevVAa.Sprint1.Task7.V17.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.YakovlevVAa.Sprint1.Task7.V17.Lib
         public double Calculate(double x, double y)
         {
             if (Math.Cos((12*y)-4) != 0)
-                    return Math.Round((1 + Math.Sin((x * x) + 1)) / (Math.Cos((12 * y) - 4)), 3);
+                    return Math.Round((1 + Math.Sin(Math.Sqrt((x * x) + 1))) / (Math.Cos((12 * y) - 4)), 3);
             else
                 return 0;
 
